532d73a2caf269161b6fcb21af1f5e5b60e4cccb
Jarowit1995 <jaroslawmazurek1995@gmail.com>
2020-03-25 18:20:35 +0100

    init components

--- habanero/src/App.tsx
+++ habanero/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
 
-const App: Function = () => <div className="App">Habanero</div>;
+const App: Function = () => <div className="App">Habanero2</div>;
 
 export default App;
