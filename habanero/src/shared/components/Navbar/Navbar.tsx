a3eee2261a75ee17a9f4364de5496e8b10990da2
Adrian Chrobak <chrobak-adrian@o2.pl>
2020-03-26 10:36:52 +0100

    Landing improvements

--- habanero/src/shared/components/Navbar/Navbar.tsx
+++ habanero/src/shared/components/Navbar/Navbar.tsx
@@ -3,19 +3,16 @@ import { Link } from "react-router-dom";
 
 const Navbar: Function = () => {
   const links: JSX.Element = (
-    <h1>
-      <Link to="/contact">
-        <div className="btn btn-danger">KONTAKT</div>
-      </Link>
-    </h1>
+    <Link to="/contact">
+      <div className="btn btn-danger">KONTAKT</div>
+    </Link>
   );
+
   return (
     <nav className="navbar bg-dark">
-      <h1>
-        <Link to="/">
-          <div className="btn btn-danger">HABANERO</div>
-        </Link>
-      </h1>
+      <Link to="/">
+        <div className="btn btn-danger">HABANERO</div>
+      </Link>
       {links}
     </nav>
   );
