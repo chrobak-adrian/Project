56ee2ac8321cfdd9dc799b91db71265743e7ad7c
Jarowit1995 <jaroslawmazurek1995@gmail.com>
2020-04-05 14:08:19 +0200

    added order content

--- habanero/src/shared/components/Order/Order.tsx
+++ habanero/src/shared/components/Order/Order.tsx
@@ -1,5 +1,76 @@
 import React from "react";
 
-const Order: React.FC = () => <section className="order">Order</section>;
+interface IProps {}
+interface IState {
+  orderType: number;
+  ingredients: {
+    cheese: 0;
+    chicken: 0;
+    tomato: 0;
+    corn: 0;
+    mushrooms: 0;
+  };
+}
+
+class Order extends React.Component<IProps, IState> {
+  state = {
+    orderType: 0,
+  } as IState;
+
+  onBuritoSelect = () => this.setState({ orderType: 0 });
+  onQuesadillaSelect = () => this.setState({ orderType: 1 });
+
+  render() {
+    const { orderType } = this.state;
+    const orderQuesadillaType: Array<string> = [
+      "Amerykańska",
+      "Azjatycka",
+      "Grecka",
+      "Meksykańska",
+    ];
+    const orderBuritoType: Array<string> = [
+      "Amerykańska",
+      "Azjatycka",
+      "Grecka",
+      "Meksykańska",
+    ];
+    const ingredients = ["Ser", "Kukurydza", "Kurczak", "Pieczarki", "Pomidor"];
+    console.log(this.state);
+    return (
+      <section className="order">
+        <div className="order-container">
+          <div className="order-nav">
+            <div className="btn btn-danger" onClick={this.onBuritoSelect}>
+              Burito
+            </div>
+            <div className="btn btn-danger" onClick={this.onQuesadillaSelect}>
+              Quesadilla
+            </div>
+          </div>
+          <div className="order-sidebar">
+            {orderType === 0
+              ? orderBuritoType.map((type: string, index: number) => (
+                  <div key={index}>{`${index + 1}. ${type}`}</div>
+                ))
+              : orderQuesadillaType.map((type: string, index: number) => (
+                  <div key={index}>{`${index + 1}. ${type}`}</div>
+                ))}
+          </div>
+          <div className="order-content">
+            <div>
+              {ingredients.map((ingidient: string, index: number) => (
+                <div key={index} className="ingredient-row">
+                  <div className="ingredient-name">{ingidient}</div>
+                  <div className="btn btn-danger">+</div>
+                  <div className="btn btn-danger">-</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Order;
